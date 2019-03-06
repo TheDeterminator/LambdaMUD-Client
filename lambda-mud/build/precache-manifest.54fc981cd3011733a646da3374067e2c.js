d266cde4100d21ef520de0f564ebca81c43e692b
Alec <alec.jordan.cs@gmail.com>
2019-03-06 13:05:51 -0500

    re-deploy after local changes

---
+++ lambda-mud/build/precache-manifest.54fc981cd3011733a646da3374067e2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f704bd978031d75452e4",
+    "url": "/static/css/main.80d19d00.chunk.css"
+  },
+  {
+    "revision": "f704bd978031d75452e4",
+    "url": "/static/js/main.f704bd97.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "250965313bb1a93a1276",
+    "url": "/static/js/2.25096531.chunk.js"
+  },
+  {
+    "revision": "24f61287c3016a7ec8d494a8852e396f",
+    "url": "/static/media/play-rpg-2.24f61287.jpg"
+  },
+  {
+    "revision": "5e2b73b0c46b9405d0cb8a705ebf582a",
+    "url": "/static/media/rpg_play_bkg.5e2b73b0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/static/media/pink_char.9f19440f.png"
+  },
+  {
+    "revision": "2755ebd83196d4c242231e92fc7e1737",
+    "url": "/index.html"
+  }
+];
